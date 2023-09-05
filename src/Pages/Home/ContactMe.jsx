825798c59fbc45aee295a48eb8b2b86a581bba10
VulliVarsha <66997407+VulliVarsha@users.noreply.github.com>
2023-09-05 04:01:06 -0400

    Update ContactMe.jsx

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,14 +1,40 @@
+import React, { useState } from 'react';
+
 export default function ContactMe() {
+  // Define state variables for each input field
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [message, setMessage] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Add your form submission logic here
+
+    // Reset input fields to empty strings after submission
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPhoneNumber('');
+    setMessage('');
+  };
+
   return (
     <section id="Contact" className="contact--section">
       <div>
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
-        <p className="text-lg">
-          Submit the form below to get in touch.
-        </p>
+        <p className="text-lg">Submit the form below to get in touch.</p>
       </div>
-      <form action="https://getform.io/f/bc61944d-92f5-4053-8de2-64cf0867405a" method="POST" className="contact--form--container">
+      <form
+        onSubmit={handleSubmit} // Attach the submit handler to the form
+        action="https://getform.io/f/bc61944d-92f5-4053-8de2-64cf0867405a"
+        method="POST"
+        className="contact--form--container"
+      >
         <div className="container">
           <label htmlFor="first-name" className="contact--label">
             <span className="text-md">First Name</span>
@@ -17,6 +43,8 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="first-name"
               id="first-name"
+              value={firstName} // Set the value from state
+              onChange={(e) => setFirstName(e.target.value)} // Update state on change
               required
             />
           </label>
@@ -27,6 +55,8 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="last-name"
               id="last-name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
               required
             />
           </label>
@@ -37,16 +67,20 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </label>
           <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">phone-number</span>
+            <span className="text-md">Phone Number</span>
             <input
               type="number"
               className="contact--input text-md"
               name="phone-number"
               id="phone-number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
               required
             />
           </label>
@@ -58,10 +92,14 @@ export default function ContactMe() {
             id="message"
             rows="8"
             placeholder="Type your message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </label>
         <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
+          <button className="btn btn-primary contact--form--btn" type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </section>
